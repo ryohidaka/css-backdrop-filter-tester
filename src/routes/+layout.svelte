d0116a9d88529f876c60a932b5520f45e2386ad2
日高 凌 <39184410+ryohidaka@users.noreply.github.com>
2023-09-13 18:48:01 +0900

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Footer from '$components/layout/Footer.svelte';
+	import Header from '$components/layout/Header.svelte';
+</script>
+
+<Header />
+<main>
+	<slot />
+</main>
+<Footer />
